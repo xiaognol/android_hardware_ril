cb14661274ce0d2bce7240e79c5f5e522c07bebe
xjljian <xjlxxx3@gmail.com>
2014-03-17 03:20:39 +0800

    Fix sim_io, available

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -309,6 +309,7 @@ typedef struct {
 } RIL_Dial;
 
 typedef struct {
+    char *aid;
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
@@ -323,6 +324,7 @@ typedef struct {
 } RIL_SIM_IO_v5;
 
 typedef struct {
+    char *aid;
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
